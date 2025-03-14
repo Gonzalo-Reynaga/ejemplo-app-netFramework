8bbfe4aa82af75c561667cc39b2c6e32e82041c3
Gonzalo Reynaga <greynaga91@gmail.com>
2025-03-14 01:58:04 -0300

    Agregado conexion a DB y carga del dataGridView

--- presentacion/frmPrincipal.Designer.cs
+++ presentacion/frmPrincipal.Designer.cs
@@ -28,20 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblFiltrar = new System.Windows.Forms.Label();
+            this.cbMarca = new System.Windows.Forms.ComboBox();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtPrecioMinimo = new System.Windows.Forms.TextBox();
+            this.txtPrecioMaximo = new System.Windows.Forms.TextBox();
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.cbBuscador = new System.Windows.Forms.ComboBox();
+            this.txtBuscador = new System.Windows.Forms.TextBox();
+            this.pbArticulo = new System.Windows.Forms.PictureBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblFiltrar
+            // 
+            this.lblFiltrar.AutoSize = true;
+            this.lblFiltrar.Location = new System.Drawing.Point(15, 64);
+            this.lblFiltrar.Name = "lblFiltrar";
+            this.lblFiltrar.Size = new System.Drawing.Size(32, 13);
+            this.lblFiltrar.TabIndex = 0;
+            this.lblFiltrar.Text = "Filtrar";
+            // 
+            // cbMarca
+            // 
+            this.cbMarca.FormattingEnabled = true;
+            this.cbMarca.Location = new System.Drawing.Point(80, 109);
+            this.cbMarca.Name = "cbMarca";
+            this.cbMarca.Size = new System.Drawing.Size(125, 21);
+            this.cbMarca.TabIndex = 1;
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(15, 112);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 2;
+            this.lblMarca.Text = "Marca";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(15, 147);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(54, 13);
+            this.lblCategoria.TabIndex = 3;
+            this.lblCategoria.Text = "Categoría";
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Location = new System.Drawing.Point(80, 144);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(125, 21);
+            this.cbCategoria.TabIndex = 4;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(15, 185);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 5;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // txtPrecioMinimo
+            // 
+            this.txtPrecioMinimo.Location = new System.Drawing.Point(30, 211);
+            this.txtPrecioMinimo.Name = "txtPrecioMinimo";
+            this.txtPrecioMinimo.Size = new System.Drawing.Size(74, 20);
+            this.txtPrecioMinimo.TabIndex = 6;
+            // 
+            // txtPrecioMaximo
+            // 
+            this.txtPrecioMaximo.Location = new System.Drawing.Point(110, 211);
+            this.txtPrecioMaximo.Name = "txtPrecioMaximo";
+            this.txtPrecioMaximo.Size = new System.Drawing.Size(78, 20);
+            this.txtPrecioMaximo.TabIndex = 7;
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.Location = new System.Drawing.Point(80, 256);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(74, 27);
+            this.btnAplicar.TabIndex = 8;
+            this.btnAplicar.Text = "Aplicar filtros";
+            this.btnAplicar.UseVisualStyleBackColor = true;
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(225, 64);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(629, 283);
+            this.dgvArticulos.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(225, 363);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(108, 33);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar artículo";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(337, 363);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(106, 33);
+            this.btnModificar.TabIndex = 11;
+            this.btnModificar.Text = "Modificar artículo";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(447, 363);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(106, 33);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar artículo";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Location = new System.Drawing.Point(224, 27);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(61, 13);
+            this.lblBuscar.TabIndex = 13;
+            this.lblBuscar.Text = "Buscar por:";
+            // 
+            // cbBuscador
+            // 
+            this.cbBuscador.FormattingEnabled = true;
+            this.cbBuscador.Location = new System.Drawing.Point(291, 24);
+            this.cbBuscador.Name = "cbBuscador";
+            this.cbBuscador.Size = new System.Drawing.Size(101, 21);
+            this.cbBuscador.TabIndex = 14;
+            // 
+            // txtBuscador
+            // 
+            this.txtBuscador.Location = new System.Drawing.Point(407, 24);
+            this.txtBuscador.Name = "txtBuscador";
+            this.txtBuscador.Size = new System.Drawing.Size(110, 20);
+            this.txtBuscador.TabIndex = 15;
+            // 
+            // pbArticulo
+            // 
+            this.pbArticulo.Location = new System.Drawing.Point(887, 64);
+            this.pbArticulo.Name = "pbArticulo";
+            this.pbArticulo.Size = new System.Drawing.Size(264, 252);
+            this.pbArticulo.TabIndex = 16;
+            this.pbArticulo.TabStop = false;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(888, 327);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(263, 20);
+            this.txtDescripcion.TabIndex = 17;
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1204, 441);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.pbArticulo);
+            this.Controls.Add(this.txtBuscador);
+            this.Controls.Add(this.cbBuscador);
+            this.Controls.Add(this.lblBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvArticulos);
+            this.Controls.Add(this.btnAplicar);
+            this.Controls.Add(this.txtPrecioMaximo);
+            this.Controls.Add(this.txtPrecioMinimo);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.cbMarca);
+            this.Controls.Add(this.lblFiltrar);
             this.Name = "frmPrincipal";
-            this.Text = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestión de artículos";
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbArticulo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFiltrar;
+        private System.Windows.Forms.ComboBox cbMarca;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtPrecioMinimo;
+        private System.Windows.Forms.TextBox txtPrecioMaximo;
+        private System.Windows.Forms.Button btnAplicar;
+        private System.Windows.Forms.DataGridView dgvArticulos;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.ComboBox cbBuscador;
+        private System.Windows.Forms.TextBox txtBuscador;
+        private System.Windows.Forms.PictureBox pbArticulo;
+        private System.Windows.Forms.TextBox txtDescripcion;
     }
 }
--- presentacion/frmPrincipal.cs
+++ presentacion/frmPrincipal.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using negocio;
+using dominio;
 
 namespace presentacion
 {
     public partial class frmPrincipal: Form
     {
+        //private List<Articulo> listaArticulos;    Para cargar el DGV, pero no sé si es necesario
         public frmPrincipal()
         {
             InitializeComponent();
@@ -19,7 +22,20 @@ namespace presentacion
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
-            //Esto es un comentario de prueba
+            cargarTabla();
+        }
+
+        private void cargarTabla()
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            dgvArticulos.DataSource = negocio.listar();
+            ocultarColumnas();
+        }
+
+        private void ocultarColumnas()
+        {
+            dgvArticulos.Columns["Id"].Visible = false;
+            dgvArticulos.Columns["UrlImagen"].Visible = false;
         }
     }
 }
